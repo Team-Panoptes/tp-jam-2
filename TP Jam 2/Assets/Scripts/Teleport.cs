9d5971a564112b22a3acdb0879f3902ed1f73131
Bastien Gorissen <kadomony@gmail.com>
2021-02-19 15:00:30 +0100

    Manage active/inactive state for the Teleport Sphere

--- TP Jam 2/Assets/Scripts/Teleport.cs
+++ TP Jam 2/Assets/Scripts/Teleport.cs
@@ -9,11 +9,16 @@ public class Teleport : MonoBehaviour
     public InputActionReference teleportAction;
     public Transform teleportSphere;
     public Transform playerTransform;
+    public Camera mainCamera;
+    private SphereManager sphereManager;
+
 
     // Start is called before the first frame update
     void Start()
     {
         teleportSphere = GameObject.FindWithTag("TeleportSphere").transform;
+        mainCamera = Camera.main;
+        sphereManager = teleportSphere.GetComponent<SphereManager>();
     }
 
     void OnEnable() {
@@ -25,7 +30,9 @@ public class Teleport : MonoBehaviour
     }
 
     void DoTeleport(InputAction.CallbackContext context) {
-        Debug.Log("TELEPORT!");
-        playerTransform.position = teleportSphere.position;
+        if (sphereManager.isActive) {
+            Debug.Log("TELEPORT!");
+            playerTransform.position = teleportSphere.position - Vector3.ProjectOnPlane(mainCamera.transform.localPosition, Vector3.up) - Vector3.up * 0.05f;
+        }
     }
 }
